056781fe69b049b68c9a0e39358a1896cd2536e2
Sam Guo <netonion@hotmail.com>
2017-08-03 15:40:14 -0400

    Exercise 20 with class

--- robot-name/robot-name.js
+++ robot-name/robot-name.js
@@ -1,22 +1,27 @@
-function Robot() {
-  this.name = this.genName();
+let names = {};
+
+class Robot {
+
+  constructor() {
+    this.reset();
+  }
+
+  genName() {
+    let name;
+
+    do {
+      name = String.fromCharCode(Math.floor(Math.random() * 26 + 'A'.charCodeAt(0)));
+      name += String.fromCharCode(Math.floor(Math.random() * 26 + 'A'.charCodeAt(0)));
+      name += Math.random().toString().slice(2, 5);
+    } while (names[name]);
+
+    names[name] = true;
+    return name;
+  }
+
+  reset() {
+    this.name = this.genName();
+  }
 }
-
-Robot.names = {}
-Robot.prototype.genName = function() {
-  let name;
-  do {
-    name = String.fromCharCode(Math.floor(Math.random() * 26 + 'A'.charCodeAt(0)));
-    name += String.fromCharCode(Math.floor(Math.random() * 26 + 'A'.charCodeAt(0)));
-    name += Math.random().toString().slice(2, 5);
-  } while (Robot.names[name])
-
-  Robot.names[name] = true;
-  return name;
-};
-
-Robot.prototype.reset = function() {
-  Robot.call(this);
-};
 
 module.exports = Robot;
