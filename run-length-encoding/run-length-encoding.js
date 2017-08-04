757aac6ec16405232a8a5778b883f235f0fd5bfd
Sam Guo <netonion@hotmail.com>
2017-08-04 14:26:29 -0400

    Run length encoding without regex

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,4 +1,33 @@
-const encode = str => str.replace(/(\w)\1+/g, (m, c) => `${m.length}${c}`);
-const decode = str => str.replace(/(\d+)(\w)/g, (_, d, c) => c.repeat(d));
+const encode = str => {
+  let res = '';
+  let prev = '';
+  let count = 0;
+  for (let c of str) {
+    if (c === prev) {
+      count++;
+    } else {
+      res += /\W/.test(prev) ? prev.repeat(count) : `${count <= 1 ? '' : count}${prev}`;
+      count = 1;
+      prev = c;
+    }
+  }
+  return res + `${count <= 1 ? '' : count}${prev}`;
+}
+
+const decode = str => {
+  let res = '';
+  let count = 0;
+  for (let c of str){
+    if (/\d/.test(c)) {
+      count = count * 10 + Number(c);
+    } else if (count) {
+      res += c.repeat(count);
+      count = 0;
+    } else {
+      res += c;
+    }
+  }
+  return res;
+}
 
 module.exports = {encode, decode};
