48e91a5f9cc30dc45881778e83c34f6dd349f720
Sam Guo <netonion@hotmail.com>
2017-08-05 19:08:23 -0400

    Roman numerals with recursion

--- roman-numerals/roman-numerals.js
+++ roman-numerals/roman-numerals.js
@@ -2,17 +2,15 @@ const mappings =
   {1000: "M", 900: "CM", 500: "D", 400: "CD",
     100: "C",  90: "XC",  50: "L",  40: "XL",
      10: "X",   9: "IX",   5: "V",   4: "IV",
-      1: "I"}
+      1: "I"};
+
+const bounds = Object.keys(mappings).reverse(); // from large to small
 
 function toRoman(n) {
-  let roman = '';
-  for (let bound of Object.keys(mappings).reverse()) {
-    while (n >= Number(bound)) {
-      roman += mappings[bound];
-      n -= Number(bound);
-    }
+  if (n <= 0) return '';
+  for (let bound of bounds) {
+    if (n >= Number(bound)) return mappings[bound] + toRoman(n - Number(bound));
   }
-  return roman;
 }
 
 module.exports = toRoman;
