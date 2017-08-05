24b5a123290635b195b03324efe4947f1ac39c18
Sam Guo <netonion@hotmail.com>
2017-08-04 21:10:32 -0400

    Sublist without String#includes shortcut

--- sublist/sublist.js
+++ sublist/sublist.js
@@ -1,3 +1,14 @@
+function isSublist(l1, l2) {
+  for (let i = 0; i <= l2.length - l1.length; i++) {
+    let j;
+    for (j = 0; j < l1.length; j++) {
+      if (l1[j] !== l2[i + j]) break;
+    }
+    if (j === l1.length) return true;
+  }
+  return false;
+}
+
 class List {
 
   constructor(list = []) {
@@ -5,11 +16,11 @@ class List {
   }
 
   compare(that) {
-    let l1 = this.list.join(" ");
-    let l2 = that.list.join(" ");
-    if (l1 === l2) return 'EQUAL';
-    if (l1.includes(l2)) return 'SUPERLIST';
-    if (l2.includes(l1)) return 'SUBLIST';
+    let l1 = this.list;
+    let l2 = that.list;
+    if (l1.length === l2.length && isSublist(l1, l2)) return 'EQUAL';
+    if (l1.length < l2.length && isSublist(l1, l2)) return 'SUBLIST';
+    if (l2.length < l1.length && isSublist(l2, l1)) return 'SUPERLIST';
     return 'UNEQUAL';
   }
 }
