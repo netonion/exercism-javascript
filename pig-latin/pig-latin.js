92abb4599eaf6ea14c06e434acf9f8b827e9b298
Sam Guo <netonion@hotmail.com>
2017-08-08 22:18:08 -0400

    Pig latin v2

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -1,15 +1,12 @@
 function translateOneWord(word) {
-  if (['a', 'e', 'i', 'o', 'u'].includes(word[0])) return word + 'ay';
+  let i = 0;
 
-  if (word.startsWith('sch') || word.startsWith('thr') || word.slice(1, 3) === 'qu') {
-    return word.slice(3) + word.slice(0, 3) + 'ay';
+  while (!['a', 'e', 'i', 'o', 'u'].includes(word[i])) {
+    if (word[i] === 'q') i++;
+    i++;
   }
 
-  if (word.startsWith('ch') || word.startsWith('th') || word.startsWith('qu')) {
-    return word.slice(2) + word.slice(0, 2) + 'ay';
-  }
-
-  return word.slice(1) + word[0] + 'ay';
+  return word.slice(i) + word.slice(0, i) + 'ay';
 }
 
 function translate(words) {
